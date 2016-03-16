0cf1ec718638e44f7a3d8620aa6c9b49a78b30d0
Alexandr Vronskiy <shandy.developer@gmail.com>
2016-03-16 21:16:22 +0200

    first commit

---
+++ config/autoload/local.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'debug' => true,
+
+    'config_cache_enabled' => false,
+
+    'some_param_for_app' => getenv('TEST_ENV_PARAM')
+];
---
+++ src/App/Action/HomePageFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Action;
+
+use Interop\Container\ContainerInterface;
+use Zend\Expressive\Router\RouterInterface;
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+class HomePageFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $router   = $container->get(RouterInterface::class);
+        $template = ($container->has(TemplateRendererInterface::class))
+            ? $container->get(TemplateRendererInterface::class)
+            : null;
+        $config = $container->get('config');
+        return new HomePageAction($router, $template, $config['some_param_for_app']);
+    }
+}
