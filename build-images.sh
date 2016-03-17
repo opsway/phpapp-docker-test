916b89920f3b6c8ad1beb7be5b310befd7b4e33e
Bogdan Savluk <savluk.bogdan@gmail.com>
2016-03-17 03:13:53 +0200

    script for building production images

---
+++ build-images.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -e
+
+# cleanup from previous build
+PHP_IMAGE=`docker build -q ./docker/php`
+
+PHP="docker run -it --rm
+       -v $(pwd):/app
+       -w /app
+       ${PHP_IMAGE}"
+
+${PHP} composer install -o
+
+# pre-build checks
+# code style
+${PHP} composer check
+
+# todo remove dev dependencies
+
+docker build -t phpapp/php-fpm -f ./docker/php-fpm-prod/Dockerfile .
+docker build -t phpapp/nginx -f ./docker/nginx-prod/Dockerfile .
+
+#docker tag phpapp/php-fpm hub.opsway.com:5000/phpapp/php-fpm
+#docker tag phpapp/nginx hub.opsway.com:5000/phpapp/nginx
